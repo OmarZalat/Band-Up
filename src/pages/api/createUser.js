60ed4a63383db448b8526bad5dace8713a24f970
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2023-04-13 23:18:46 +0300

    hashed password

--- src/pages/api/createUser.js
+++ src/pages/api/createUser.js
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 
 const prisma = new PrismaClient();
 
@@ -14,13 +15,17 @@ export default async function handler(req, res) {
     subscribe,
   } = req.body.formData;
 
+  const bcrypt = require("bcrypt");
+  const saltRounds = 10;
+  const hashedPassword = bcrypt.hashSync(password, saltRounds);
+
   try {
     const response = await prisma.userData.create({
       data: {
         FName: firstName,
         LName: lastName,
         email,
-        password,
+        password: hashedPassword,
         DOB,
         gender,
         country,
