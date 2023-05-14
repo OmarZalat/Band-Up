607e2c1407d3cd201fc3eb20544d4aae964748dd
OmarZalat <zaltomar18@gmail.com>
2023-05-14 22:04:55 +0300

    updated the featured tab in the my band page

---
+++ src/components/MyBand/myBandFeaturedTab.js
@@ -0,0 +1,15 @@
+import MyBandPostingUI from "../UI/myBandPostingUI";
+
+function MyBandFeaturedTab() {
+  return (
+    <>
+      <div id="band_featured_wrapper">
+        <div id="band_featured_card">
+          <MyBandPostingUI />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MyBandFeaturedTab;
