a9d697ee1478824fe7e38230e1f4a9e8c239b955
HarakeSenpai <mohamad.harakeh2001@gmail.com>
2023-05-21 21:39:03 +0300

    commiting

--- src/pages/api/fetchUserData.js
+++ src/pages/api/fetchUserData.js
@@ -1,43 +1,123 @@
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   try {
-    const { email, password } = req.body.formData;
-    console.log(password);
-    const saltRounds = 10;
-    const hashedPassword = bcrypt.hashSync(password, saltRounds);
-    console.log(hashedPassword);
-    const response = await prisma.userData.findFirst({
-      select: {
-        id: true,
-        FName: true,
-        LName: true,
-        email: true,
-        DOB: true,
-        gender: true,
-        country: true,
-        newsletterMember: true,
-        emailVerification: true,
-        password: true,
-        username: true,
-        bio: true,
-        bandDataId: true,
-        role: true,
-      },
-      where: {
-        email,
-      },
-    });
-    const passwordMatch = bcrypt.compareSync(password, response.password);
-    console.log(passwordMatch);
+    const userToken = req.cookies.userToken;
 
-    if (passwordMatch) {
-      res.json(response);
-    } else {
-      res.send(false);
+    if (userToken) {
+      const response = await prisma.session.findFirst({
+        select: {
+          user: {
+            select: {
+              id: true,
+              FName: true,
+              LName: true,
+              email: true,
+              DOB: true,
+              gender: true,
+              country: true,
+              newsletterMember: true,
+              emailVerification: true,
+              password: true,
+              username: true,
+              bio: true,
+              bandDataId: true,
+              role: true,
+            },
+          },
+        },
+        where: {
+          sessionToken: userToken,
+        },
+      });
+      res.send(response);
+    } else if (req.body?.formData.email && req.body?.formData.password) {
+      const { email, password } = req.body.formData;
+      console.log(password);
+      const saltRounds = 10;
+      const hashedPassword = bcrypt.hashSync(password, saltRounds);
+      console.log(hashedPassword);
+
+      const response = await prisma.userData.findFirst({
+        select: {
+          id: true,
+          FName: true,
+          LName: true,
+          email: true,
+          DOB: true,
+          gender: true,
+          country: true,
+          newsletterMember: true,
+          emailVerification: true,
+          password: true,
+          username: true,
+          bio: true,
+          bandDataId: true,
+          role: true,
+        },
+        where: {
+          email,
+        },
+      });
+      const passwordMatch = bcrypt.compareSync(password, response.password);
+      console.log(passwordMatch);
+
+      if (passwordMatch) {
+        //Generate User Token
+        const sessionToken = jwt.sign(response, process.env.SECRET_KEY, {
+          expiresIn: "1h",
+        });
+        //Add token to user object
+        const session = await prisma.session.findFirst({
+          where: {
+            userDataId: response.id,
+          },
+        });
+        const expires = new Date();
+        expires.setHours(expires.getHours() + 1); // Set cookie expiration time to 1 hour from now
+
+        if (session) {
+          await prisma.session.update({
+            where: {
+              userDataId: response.id,
+            },
+            data: {
+              createdAt: new Date(),
+              expiry: expires,
+              sessionToken,
+            },
+          });
+        } else {
+          await prisma.session.create({
+            data: {
+              expiry: expires,
+              sessionToken,
+              user: {
+                connect: {
+                  id: response.id,
+                },
+              },
+            },
+          });
+        }
+
+        // Set the cookie
+        res.setHeader(
+          "Set-Cookie",
+          `userToken=${sessionToken}; Path=/; Expires=${expires.toUTCString()}; HttpOnly`
+        );
+
+        res.json(response);
+      } else {
+        res.send(false);
+      }
+
+      // Return the response
+      res.status(200).json({ message: "Cookie set!" });
     }
   } catch (e) {
     console.log(e);
