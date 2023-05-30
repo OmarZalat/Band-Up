cdc95df239b1521b27a269daa49a2f03a739a2dc
OmarZalat <zaltomar18@gmail.com>
2023-05-30 21:12:26 +0300

    added loading state when fetching interests in profile page

--- src/components/Profile/profileInterests.js
+++ src/components/Profile/profileInterests.js
@@ -1,16 +1,42 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 
 function ProfileInterests() {
   const interestsContentInfoRef = useRef(null);
+  const [userInterests, setUserInterests] = useState([]);
 
+  // console.log(userInterests);
   useEffect(() => {
-    const interestsContentInfo = interestsContentInfoRef.current;
+    // Fetch user's interests from the backend
+    fetchUserInterests();
 
-    interestsContentInfo.addEventListener("wheel", (event) => {
-      event.preventDefault();
-      interestsContentInfo.scrollLeft += event.deltaY;
-    });
+    const interestsContentInfo = interestsContentInfoRef.current;
+    interestsContentInfo.addEventListener("wheel", handleWheelEvent);
+
+    return () => {
+      interestsContentInfo.removeEventListener("wheel", handleWheelEvent);
+    };
   }, []);
+
+  const fetchUserInterests = async () => {
+    try {
+      // Make an API request to fetch user's interests
+      const response = await fetch("/api/fetchUserData"); // Replace "/api/user/interests" with your actual API endpoint
+
+      if (response.ok) {
+        const interests = await response.json();
+        setUserInterests(interests);
+      } else {
+        console.log("Failed to fetch user interests.");
+      }
+    } catch (error) {
+      console.log("Error while fetching user interests:", error);
+    }
+  };
+
+  const handleWheelEvent = (event) => {
+    event.preventDefault();
+    interestsContentInfoRef.current.scrollLeft += event.deltaY;
+  };
 
   return (
     <>
@@ -18,7 +44,15 @@ function ProfileInterests() {
         <div id="interests">
           <div id="interests_content_label">interests</div>
           <div id="interests_content_info" ref={interestsContentInfoRef}>
-            {/* should be dynamic */}
+            {userInterests.length > 0 ? (
+              userInterests.map((interest) => (
+                <div key={interest.id} className="interest-item">
+                  {interest.name}
+                </div>
+              ))
+            ) : (
+              <div>Loading interests...</div>
+            )}
           </div>
         </div>
       </div>
