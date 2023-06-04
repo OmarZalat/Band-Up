d1fc53cd45c5e1b4e08afae65268c50e8b4af2c4
Sam39 <samzahreddine@gmail.com>
2023-06-04 17:49:44 +0300

    fetching band data

--- src/pages/api/joinBand.js
+++ src/pages/api/joinBand.js
@@ -3,7 +3,7 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
-  const { userID, bandID, verificationStatus } = req.body;
+  const { userID, bandID, verificationStatus, positionID } = req.body;
   if (verificationStatus) {
     try {
       const response = await prisma.userData.update({
@@ -13,6 +13,11 @@ export default async function handler(req, res) {
         data: {
           bandDataId: bandID,
           role: "MEMBER",
+          position: {
+            connect: {
+              id: positionID,
+            },
+          },
         },
       });
       await prisma.bandData.update({
