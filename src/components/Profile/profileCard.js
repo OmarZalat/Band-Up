96afcd6c6cbe94aa7ed89e13627700b73e0a06c8
OmarZalat <zaltomar18@gmail.com>
2023-06-06 23:50:25 +0300

    added friends counter; fixed modal size and interests selection; added band name and position in profile page

--- src/components/Profile/profileCard.js
+++ src/components/Profile/profileCard.js
@@ -8,44 +8,43 @@ function ProfileCard({ FName, bio, profilePicture }) {
   // Check if the user is viewing their own profile
   const isOwnerProfile = true; // Replace this with your logic to determine if it's the owner's profile
   const { user, setUser } = useContext(UserContext);
+  console.log(user.follower);
+
+  const followerCount = user.follower ? user.follower.length : 0;
 
   return (
-    <>
-      <div id="user_card">
-        <div
-          id="user_card_profile_picture"
-          style={{
-            backgroundImage: user.image
-              ? `url(${user.image})`
-              : `url("${defaultBg}")`,
-          }}
-        >
-          {isOwnerProfile ? null : ( // Display nothing when it's the owner's profile
-            <div id="user_card_button_wrapper">
-              <div id="follow_unfollow_button">follow {FName}</div>
-              <div id="more_button">▪▪▪</div>
-            </div>
-          )}
-        </div>
-        <div id="user_card_details_wrapper">
-          <div id="user_card_about">
-            <h1>About</h1>
-            <p>{bio}</p>
+    <div id="user_card">
+      <div
+        id="user_card_profile_picture"
+        style={{
+          backgroundImage: user.image
+            ? `url(${user.image})`
+            : `url("${defaultBg}")`,
+        }}
+      >
+        {isOwnerProfile ? null : (
+          <div id="user_card_button_wrapper">
+            <div id="follow_unfollow_button">follow {FName}</div>
+            <div id="more_button">▪▪▪</div>
           </div>
-        </div>
-
-        <div id="user_card_friends">
-          <div id="user_card_friends_label">friends</div>
-          {/* should be dynamic */}
-          <div id="user_card_friends_number">0</div>
-        </div>
-        <div id="user_card_posts">
-          <div id="user_card_posts_label">posts</div>
-          {/* should be dynamic */}
-          <div id="user_card_posts_number">0</div>
+        )}
+      </div>
+      <div id="user_card_details_wrapper">
+        <div id="user_card_about">
+          <h1>About</h1>
+          <p>{bio}</p>
         </div>
       </div>
-    </>
+
+      <div id="user_card_friends">
+        <div id="user_card_friends_label">friends</div>
+        <div id="user_card_friends_number">{followerCount}</div>
+      </div>
+      <div id="user_card_posts">
+        <div id="user_card_posts_label">posts</div>
+        <div id="user_card_posts_number">0</div>
+      </div>
+    </div>
   );
 }
 
